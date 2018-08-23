f79227633fe6f3a486f57beb599f23ddc8378721
klausthansen <vikse58@gmail.com>
2018-08-23 15:42:00 +0200

    opgave

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variabel2 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+            Console.WriteLine(heltal);
+
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
+
+            Console.ReadKey();
+        }
+    }
+}
--- variabler/Program.cs
+++ variabler/Program.cs
@@ -10,8 +10,37 @@ namespace variabler
     {
         static void Main(string[] args)
         {
-        //
-        // test2
+            int antalMaanederPrAar = 12;
+            System.Int32 a = 1;
+            int a2 = 2;
+
+
+            Console.WriteLine(a);
+            Console.WriteLine("a =  " +a);
+            Console.WriteLine($"a =  {a}");
+
+
+            int y = 10;
+
+            //instans medlem//
+            Console.WriteLine(y.ToString);
+            //statisk medlem//
+            Console.WriteLine(int.MaxValue);
+
+            double z = 7971343.14158394080;
+
+            Console.WriteLine(z);
+            Console.WriteLine(z.ToString());
+            Console.WriteLine(z.ToString("N2"));
+            Console.WriteLine(z.ToString("N3"));
+            Console.WriteLine(z.ToString("F2"));
+            Console.WriteLine(z.ToString("C2"));
+
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
